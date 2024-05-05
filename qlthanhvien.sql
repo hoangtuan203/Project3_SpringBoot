d94317ae26f00cd0398a92f9673ec27f80b12a62
Tuan <nguyenhoangtuan12102003@gmail.com>
2024-05-05 19:33:01 +0700

    update sql

---
+++ qlthanhvien.sql
@@ -0,0 +1,171 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 05, 2024 at 02:31 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `qlthanhvien`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `thanhvien`
+--
+
+CREATE TABLE `thanhvien` (
+  `MaTV` int(11) NOT NULL,
+  `HoTen` varchar(100) NOT NULL,
+  `Khoa` varchar(100) DEFAULT NULL,
+  `Nganh` varchar(100) DEFAULT NULL,
+  `SDT` varchar(15) DEFAULT NULL,
+  `Email` varchar(25) DEFAULT NULL,
+  `Password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- Dumping data for table `thanhvien`
+--
+
+INSERT INTO `thanhvien` (`MaTV`, `HoTen`, `Khoa`, `Nganh`, `SDT`, `Email`, `Password`) VALUES
+(1120150184, 'Trần Thị Nữ', 'GDTH', 'GDTH', '0123456781', 'tranthinu@gmail.com', '1120150184'),
+(1121530087, 'Trần Thiếu Nam', 'TLH', 'QLGD', '0123456782', 'tranthieunam@gmail.com', '1121530087'),
+(1123330257, 'Ngô Tuyết Nhi', 'QTKD', 'QTKD', '0123456783', 'ngotuyetnhi@gmail.com', '1123330257'),
+(2147483647, 'Nguyễn Văn Nam', 'CNTT', 'HTTT', '0123456784', 'nguyenvannam@gmail.com', '2147483647');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `thietbi`
+--
+
+CREATE TABLE `thietbi` (
+  `MaTB` int(11) NOT NULL,
+  `TenTB` varchar(100) NOT NULL,
+  `MoTaTB` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- Dumping data for table `thietbi`
+--
+
+INSERT INTO `thietbi` (`MaTB`, `TenTB`, `MoTaTB`) VALUES
+(1000001, 'Micro', 'Micro không dây MS2023'),
+(1000002, 'Micro', 'Micro không dây MS2024'),
+(1000003, 'Bảng điện tử', 'Bản điện tử trình chiếu');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `thongtinsd`
+--
+
+CREATE TABLE `thongtinsd` (
+  `MaTT` int(11) NOT NULL,
+  `MaTV` int(11) NOT NULL,
+  `MaTB` int(11) DEFAULT NULL,
+  `TGVao` datetime DEFAULT NULL,
+  `TGMuon` datetime DEFAULT NULL,
+  `TGTra` datetime DEFAULT NULL,
+  `TgDatCho` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- Dumping data for table `thongtinsd`
+--
+
+INSERT INTO `thongtinsd` (`MaTT`, `MaTV`, `MaTB`, `TGVao`, `TGMuon`, `TGTra`, `TgDatCho`) VALUES
+(1, 1120150184, NULL, '2024-01-05 09:00:00', NULL, NULL, NULL),
+(2, 1123330257, 1000001, NULL, '2024-02-12 10:00:32', '2024-02-12 14:00:00', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `xuly`
+--
+
+CREATE TABLE `xuly` (
+  `MaXL` int(11) NOT NULL,
+  `MaTV` int(11) NOT NULL,
+  `HinhThucXL` varchar(250) DEFAULT NULL,
+  `SoTien` int(11) DEFAULT NULL,
+  `NgayXL` datetime DEFAULT NULL,
+  `TrangThaiXL` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- Dumping data for table `xuly`
+--
+
+INSERT INTO `xuly` (`MaXL`, `MaTV`, `HinhThucXL`, `SoTien`, `NgayXL`, `TrangThaiXL`) VALUES
+(1, 1121530087, 'Khóa thẻ 1 tháng', NULL, '2023-09-12 08:00:00', 0),
+(2, 2147483647, 'Khóa thẻ 2 tháng', NULL, '2023-09-12 08:00:00', 0),
+(3, 1123330257, 'Bồi thường mất tài sản', 300000, '2023-09-12 08:00:00', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `thanhvien`
+--
+ALTER TABLE `thanhvien`
+  ADD PRIMARY KEY (`MaTV`);
+
+--
+-- Indexes for table `thietbi`
+--
+ALTER TABLE `thietbi`
+  ADD PRIMARY KEY (`MaTB`);
+
+--
+-- Indexes for table `thongtinsd`
+--
+ALTER TABLE `thongtinsd`
+  ADD PRIMARY KEY (`MaTT`),
+  ADD KEY `MaTV` (`MaTV`,`MaTB`),
+  ADD KEY `MaTB` (`MaTB`);
+
+--
+-- Indexes for table `xuly`
+--
+ALTER TABLE `xuly`
+  ADD PRIMARY KEY (`MaXL`),
+  ADD KEY `MaTV` (`MaTV`),
+  ADD KEY `MaTV_2` (`MaTV`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `thongtinsd`
+--
+ALTER TABLE `thongtinsd`
+  ADD CONSTRAINT `thongtinsd_ibfk_1` FOREIGN KEY (`MaTV`) REFERENCES `thanhvien` (`MaTV`),
+  ADD CONSTRAINT `thongtinsd_ibfk_2` FOREIGN KEY (`MaTB`) REFERENCES `thietbi` (`MaTB`);
+
+--
+-- Constraints for table `xuly`
+--
+ALTER TABLE `xuly`
+  ADD CONSTRAINT `xuly_ibfk_1` FOREIGN KEY (`MaTV`) REFERENCES `thanhvien` (`MaTV`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ src/main/java/com/example/project3/service/RegisterService.java
@@ -0,0 +1,2 @@
+package com.example.project3.service;public class RegisterService {
+}
